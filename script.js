7cd6151d78f969f360d9d855e38ff5d07a4103a2
SyerAru <syeraru@gmail.com>
2023-02-27 19:58:13 -0700

    Create randomizer function for Action

--- script.js
+++ script.js
@@ -19,3 +19,28 @@ function generateLuck() {
 }
 
 console.log(generateLuck())
+
+function generateAction() {
+    action = Math.floor(Math.random() * 8)
+
+    switch(action) {
+        case 0:
+            return actionStart + ' pursue your dreams';
+        case 1:
+            return actionStart + ' stay home today';
+        case 2:
+            return actionStart + ' help someone out';
+        case 3:
+            return actionStart + ' drink a warm beverage';
+        case 4:
+            return actionStart + ' spend time outside';
+        case 5:
+            return actionStart + ' try a new food';
+        case 6:
+            return actionStart + ' get more rest';
+        case 7:
+            return actionStart + ' find a new hobby'; 
+    }
+}
+
+console.log(generateAction())
