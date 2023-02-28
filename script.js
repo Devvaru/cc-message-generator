c2ea60782a42e34fb9ba210889c7df9ffdc0fb2a
SyerAru <syeraru@gmail.com>
2023-02-27 20:04:56 -0700

    Refactor function for Luck generator

--- script.js
+++ script.js
@@ -9,13 +9,14 @@ const goalsStart = 'You will';
 function generateLuck() {
     luck = Math.floor(Math.random() * 3);
 
-    if (luck === 0) {
-        return luckStart + ' is on your side';
-    } else if (luck === 1) {
-        return luckStart + ' is not on your side';
-    } else if (luck == 2) {
-        return luckStart + ' will not be necessary';
-    };
+    switch(luck) {
+        case 0:
+            return luckStart + ' is on your side';
+        case 1:
+            return luckStart + ' is not on your side';
+        case 2:
+            return luckStart + ' will not be necessary';
+    }
 }
 
 console.log(generateLuck())
